c7f672f8b8366e2470d6a41b81f74660c1c185cb
crayonluffy <crayonluffy@gmail.com>
2026-01-21 13:50:33 +0000

    feat: add typing effect utility class

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
 // js/main.js
+class TypeWriter {
+    constructor(element, text, speed = 50) {
+        this.element = element;
+        this.text = text;
+        this.speed = speed;
+        this.index = 0;
+    }
+
+    type() {
+        return new Promise((resolve) => {
+            const typeChar = () => {
+                if (this.index < this.text.length) {
+                    this.element.textContent += this.text.charAt(this.index);
+                    this.index++;
+                    setTimeout(typeChar, this.speed);
+                } else {
+                    resolve();
+                }
+            };
+            typeChar();
+        });
+    }
+}
+
+// Utility to create blinking cursor
+function addCursor(element) {
+    const cursor = document.createElement('span');
+    cursor.className = 'cursor';
+    cursor.textContent = '_';
+    element.appendChild(cursor);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('CV site initialized');
 });
